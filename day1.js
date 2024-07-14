9c348d46634900b83bf2e05166c2138055e2c884
Biswarup Mahato <107302651+biswarupmahato7@users.noreply.github.com>
2024-07-14 23:03:15 +0530

    first commit

---
+++ day1.js
@@ -0,0 +1,39 @@
+// Task -1 
+// let num = 9
+// var name = 'Biswarup'
+
+// console.log(num)
+// console.log(name)
+
+//Task -2
+// const result = true
+// console.log(typeof(result));
+
+//task-3
+// let myObj = {
+//     day1 : "Monday",
+//     day2 : "Tuesday"
+// }
+
+// let myArr = [1,2,3,4,5]
+
+// let name = "Biswarup"
+// let num = 2
+
+// console.log(typeof(myObj));
+// console.log(myObj);
+// console.log(typeof(myArr));
+// console.log(typeof(name));
+// console.log(typeof(num));
+
+//task -4
+// let num = 9 
+// console.log(num);
+// num = 10 
+// console.log(num)
+
+//task- 5
+const num  = 7
+console.log(num);
+num = 9
+console.log(num);
