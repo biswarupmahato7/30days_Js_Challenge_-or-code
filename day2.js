d70cb1c26ef4125c795ade638f314b60937b2ae3
Biswarup Mahato <107302651+biswarupmahato7@users.noreply.github.com>
2024-07-14 23:59:11 +0530

    Day2

---
+++ day2.js
@@ -0,0 +1,85 @@
+//task -1
+//add
+// let addition = function add(a,b){
+//     return a + b;
+
+// }
+
+// console.log(`Sum is ${addition(5,4)}`);
+// // sub
+
+// let subtract  = ((a,b)=>{
+//     return a-b
+
+// })
+// //mul
+
+// console.log(`Subtraction is ${subtract(10,5)}`);
+
+// function multiplication (a,b){
+//     return a * b
+
+// }
+// //div
+
+// console.log(`Multiplication is ${multiplication(4,4)}`);
+
+// function divide (a,b){
+//     console.log(`Division is ${a/b}`);
+// }
+// divide(100,10)
+// //rem
+
+// let reminder = function ( a , b){
+//     console.log(`Reminder is ${a % b}`);
+
+// }
+// reminder(100,9)
+
+//Task -2 
+
+// let b = 4
+// let a = 5
+// console.log(a+=b);
+
+//Task -3
+
+// let a = 5
+// const b = 4
+// let c = 5
+// console.log(a<b);
+// console.log(a>b);
+// console.log(a<=b);
+// console.log(a>=c);
+// console.log(a==b);
+// console.log(a==c);
+
+//task -4 
+
+// let a = 5
+// const b = 4
+// let c = 6
+// let d = 7
+
+// if(a>b & a<c){
+//     console.log("Both condition true");
+
+// }
+
+// if(a>b || d<c){
+//     console.log("Only one condition true");
+
+// }
+
+// if(a != b){
+//     console.log("Values are not equal");
+
+// }
+
+// task -5
+
+let a = -5
+let result = (a<0) ? "Negative" : "Positive"
+console.log(`This is a ${result} number`);
+
+
