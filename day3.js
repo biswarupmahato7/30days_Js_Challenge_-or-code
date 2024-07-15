a65010aeafa851f58505de32b3d02799dec34fd0
Biswarup Mahato <107302651+biswarupmahato7@users.noreply.github.com>
2024-07-16 00:07:47 +0530

    Day-3

---
+++ day3.js
@@ -0,0 +1,100 @@
+//Task - 1
+// let num = 0
+// if(num<0){
+//     console.log("Negative Number");
+// }else if(num>0){
+//     console.log("Positive Number");
+// }else{
+//     console.log("Zero");
+// }
+
+//Task-2
+
+// let age = 23
+// if(age<17){
+//     console.log("Not Eligible for Vote");
+// }else{
+//     console.log("You are Eligible For Vote");
+// }
+
+// task-3
+
+// let num1 = 8
+// let num2 = 12
+// let num3 = 10
+
+// if(num1 > num2 && num1 > num3){
+//     console.log("Num1 is larger ");
+// }else if(num2 > num1 && num2 > num3){
+//     console.log("Num2 is larger");
+// }else{
+//     console.log("Num3 is Larger");
+// }
+
+//task - 4
+
+// let day = 3
+// switch (day) {
+//     case 1: console.log("Monday");
+        
+//         break;
+
+//     case 2: console.log("Tuesday");
+        
+//         break;
+
+//     case 3: console.log("Wednesday");
+        
+//         break;
+//     case 4: console.log("Thursday");
+        
+//         break;
+//     case 5: console.log("Friday");
+        
+//         break;
+//     case 6: console.log("Saturday");
+//         break;
+//     case 7: console.log("Sunday");
+//         break;
+
+// }
+
+//task- 5
+
+// let marks = 70
+
+// switch (true) {
+//     case (marks < 40):
+//         console.log("Your Grade is F");
+//         break;
+//     case (marks >= 40 && marks < 50):
+//         console.log("Your Grade is D");
+//         break;
+//     case (marks >= 50 && marks < 65):
+//         console.log("Your Grade is C");
+//         break;
+//     case (marks >= 65 && marks < 85):
+//         console.log("Your Grade is B");
+//         break;
+//     case (marks >= 85 && marks <= 100):
+//         console.log("Your Grade is A");
+//         break;
+//     default:
+//         console.log("Invalid marks");
+// }
+
+//task - 6
+
+// let num = 7;
+
+// console.log(num % 2 === 0 ? "Even Number" : "Odd Number");
+
+//task-7
+
+// let year = 2004
+
+// if((year % 4 === 0 && year % 100 !==0 )|| year % 400 ===0 ){
+//     console.log("It's a Leap Year");
+// }else{
+//     console.log("It's not a Leap Year");
+// }
